a838a578db48edad25d9feca9bd6e2dc08f9f9cb
frousseu <francois.rousseu@usherbrooke.ca>
2023-11-28 19:16:04 -0500

    init new

---
+++ graphics.R
@@ -0,0 +1,105 @@
+
+library(data.table)
+library(magick)
+library(sf)
+library(terra)
+library(rmapshaper)
+library(geodata)
+library(magick)
+library(berryFunctions)
+
+
+###
+gbif<-fread("C:/Users/franc/Documents/floreqc/gbif/0021817-231002084531237.csv")
+gbif[,date:=eventDate]
+gbif[,jul:=as.integer(format(as.Date(date),"%j"))]
+gbif[,year:=substr(date,1,4)]
+
+gbif[,speciesgbif:=species]
+spnames<-unique(gbif$scientificName)
+sp<-sapply(strsplit(spnames," "),function(i){
+  g<-match("var.",i)
+  if(is.na(g)){
+    paste(i[1:2],collapse=" ")
+  }else{
+    paste(i[1:4],collapse=" ")
+  }
+})
+spnames<-data.table(scientificName=spnames,sp=sp)
+gbif<-gbif[spnames,on="scientificName"]
+#gbif[,species:=sp]
+
+gbif[,species:=d$species[match(taxon$acceptedNameUsageID[match(gbif$sp,taxon$sp)],d$taxonID)],]
+gbif[is.na(species),species:=speciesgbif]
+
+#setdiff(d$species,gbif$species)
+#table(gbif$species[grep("Adiantum",gbif$species)])
+#table(d$species[grep("Adiantum",d$species)])
+#unique(gbif[grep("Elymus",gbif$sp),c("speciesgbif","sp","species"),with=TRUE])
+
+
+dates<-format(as.Date(1:365,origin="1970-01-01")-1,"%Y-%m-%d")
+brks<-as.integer(format(as.Date(dates[c(which(substr(dates,9,10)%in%c("01")),365)]),"%j"))
+brks<-sort(c(brks,brks[-length(brks)]+diff(brks)/2))
+
+
+spnames<-unique(gbif$scientificName)
+alt_names<-sapply(strsplit(spnames," "),function(i){
+  g<-match("var.",i)
+  if(is.na(g)){
+    paste(i[1:2],collapse=" ")
+  }else{
+    paste(i[1:4],collapse=" ")
+  }
+})
+
+
+
+
+sp<-unique(d$species[d$species%in%gbif$species])#[1]
+
+for(i in sp){
+  print(i)
+  x<-gbif[species==i,"jul",]
+  if(nrow(x)==0){next}
+  h<-hist(x$jul[x$jul<366],breaks=brks,plot=FALSE)
+  #plot(density(d$jul,na.rm=TRUE))
+  pheno<-density(gbif$jul,na.rm=TRUE,adjust=3,from=1,to=365)
+  #h<-lapply(h,"[",c(5:25))
+  labs<-gsub("\\.","",format(as.Date(h$mids,origin="1970-01-01"),"%b"))
+
+  path<-paste0("C:/Users/franc/Documents/floreqc/images/",gsub(" ","_",i),"_pheno.png")
+  png(path,units="cm",width=9,height=6,res=300)
+  par(mar=c(3,2,3,0.5),bg="white")
+
+  plot(0,0,xlim=c(60,335),ylim=c(0,max(c(5,h$counts))*1.01),type="n",xaxt="n",yaxt="n",xaxs = 'i',yaxs = 'i',axes=FALSE)
+  abline(v=brks,lty=3,col="grey80")
+  abline(h=pretty(c(h$counts,5)),lty=3,col="grey80")
+  invisible(lapply(seq_along(h$counts),function(i){
+    offset<-0.75
+    roundedRect(xleft=brks[i]+offset,xright=brks[i+1]-offset,ybottom=0,ytop=h$counts[i],col="forestgreen",border=NA,corners=c(2,3),rounding=0.0,bothsame=FALSE,devcorrect=TRUE)
+  }))
+  alongx<-pheno$x
+  alongy<-(pheno$y/max(pheno$y))*max(c(h$counts,5))
+  polygon(c(alongx,rev(alongx)),c(alongy,rep(0,length(alongy))),col=adjustcolor("darkgreen",0.15),border=NA)
+  lines(alongx,alongy,col=adjustcolor("darkgreen",0.35))
+  #invisible(lapply(seq_along(h$counts),function(i){
+  #  offset<-0.75
+  #  roundedRect(xleft=brks[i]+offset,xright=brks[i+1]-offset,ybottom=0,ytop=h$counts[i],col=adjustcolor("forestgreen",0.25),border=NA,corners=c(2,3),rounding=0.15)
+  #}))
+  months<-brks[seq(2,length(brks),by=2)]
+  minus<--c(1,2,length(months))
+  months<-months[minus]
+  monthlims<-brks[-seq(2,length(brks),by=2)]
+  monthlims<-monthlims[-c(1:2)]
+  text(months,rep(0,length(months)),label=unique(labs)[minus],srt=0,adj=c(0.5,1.5),cex=0.65,xpd=TRUE)
+  axis(1,at=monthlims,labels=rep("",length(monthlims)),tcl=-0.7,lwd=0,lwd.ticks=1.0,col.ticks="grey80",xpd=TRUE)
+  mtext(side=2,line=1,font=2,text="Nb d'observations",cex=0.75)
+  axis(2,at=pretty(c(h$counts,5))[-1],mgp=c(2,0.05,0),tcl=0,las=2,col="grey50",cex.axis=0.5,lwd=0)
+  legend("top",legend=c("Observations","Toutes les espèces"),ncol=2,pch=22,pt.cex=1.25,col=c("forestgreen",adjustcolor("darkgreen",0.35)),pt.bg=c("forestgreen",adjustcolor("darkgreen",0.15)),cex=0.65,bty="n",inset=c(0.05,-0.20),xpd=TRUE,pt.lwd=1.25)
+
+  dev.off()
+  image_read(path) |> image_trim() |> image_border("10x10",color="white") |> image_write(path)
+}
+file.show(list.files(dirname(path),full=TRUE)[1])
+
---
+++ maps.R
@@ -0,0 +1,142 @@
+
+library(geodata)
+library(sf)
+library(rmapshaper)
+library(rnaturalearth)
+library(magick)
+library(foreach)
+library(doParallel)
+library(data.table)
+library(future)
+library(future.apply)
+
+d<-fread("C:/Users/franc/Documents/floreqc/plants.csv")
+
+gbif<-fread("C:/Users/franc/Documents/floreqc/gbif/0021817-231002084531237.csv")
+
+gbif[,speciesgbif:=species]
+spnames<-unique(gbif$scientificName)
+sp<-sapply(strsplit(spnames," "),function(i){
+  g<-match("var.",i)
+  if(is.na(g)){
+    paste(i[1:2],collapse=" ")
+  }else{
+    paste(i[1:4],collapse=" ")
+  }
+})
+spnames<-data.table(scientificName=spnames,sp=sp)
+gbif<-gbif[spnames,on="scientificName"]
+#gbif[,species:=sp]
+
+gbif[,species:=d$species[match(taxon$acceptedNameUsageID[match(gbif$sp,taxon$sp)],d$taxonID)],]
+gbif[is.na(species),species:=speciesgbif]
+
+gbif<-st_as_sf(gbif,coords=c("decimalLongitude","decimalLatitude"),crs=4326)
+gbif<-st_transform(gbif,32618)
+
+can<-gadm("CAN",path="C:/Users/franc/Documents/floreqc/qc.gpkg") |> st_as_sf()
+
+# Downloads polygons using package geodata
+#can<-gadm("CAN",level=1,path=getwd()) |> st_as_sf()
+can<-st_transform(can,32618)
+
+# keep Québec and bordering provinces/states as a buffer
+region<-can[can$NAME_1%in%c("Québec"),]
+
+# split NF into different polygons
+labrador<-ms_explode(can[can$NAME_1%in%c("Newfoundland and Labrador"),])
+labrador<-labrador[which.max(st_area(labrador)),] # keep Labarador
+region<-rbind(region,labrador)
+
+# Add it to the study region
+region<-rbind(region,labrador)
+
+# Simplify polygons to make things faster
+region<-ms_simplify(region,0.02)
+region<-st_union(region) |> st_as_sf()
+
+# lakes
+lakes<-ne_download(scale="large",type="lakes",destdir=getwd(),category="physical",returnclass="sf") |> st_transform(32618)
+#lakes<-ne_download(scale="large",type="rivers_lake_centerlines",destdir=getwd(),category="physical",returnclass="sf") |> st_transform(32618)
+lakes<-st_filter(lakes,region)
+lakes<-ms_simplify(lakes,0.05)
+lakes<-st_intersection(lakes,region)
+
+lim<-c(min(st_bbox(region)[c(2,4)]),5700000)
+
+bbox<-st_bbox(region)
+bbox[4]<-5700000
+
+south<-st_crop(st_intersection(region,lakes),bbox)
+buff<-st_sym_difference(st_as_sf(st_as_sfc(st_bbox(st_buffer(region,150000)))),st_buffer(region,150000))
+buffl<-st_buffer(region,150000)
+sp<-unique(d$species[d$species%in%gbif$species])
+#sp<-"Anthoxanthum nitens"
+
+cl<-makeCluster(14)
+registerDoParallel(cl)
+foreach(i=sp,.packages=c("sf","terra","magick")) %dopar% {
+#plan(multisession,workers=14)
+#l<-future_lapply(sp,function(i){
+  print(i)
+
+  plotQC<-function(region,ylim=NULL,cex=0.25){ # plotting function for the study area
+    par(mar=c(0,0,0,0),bg="white")
+    #plot(st_geometry(qc),col="grey99",border=NA)
+    plot(st_geometry(region),lwd=0.1,col="grey85",border="grey60",ylim=ylim)
+    plot(st_geometry(lakes),col="grey99",border="grey75",lwd=0.05,add=TRUE)
+    plot(st_geometry(x),pch=21,bg=adjustcolor("forestgreen",0.5),col=adjustcolor("forestgreen",0.9),lwd=0.3,cex=cex,add=TRUE)
+  }
+
+
+  x<-gbif[gbif$species==i,]
+  path<-paste0("C:/Users/franc/Documents/floreqc/images/",gsub(" ","_",i),"_map.png")
+  png(gsub("_map","_map1",path),units="cm",width=3,height=1.5,res=500)
+  plotQC(region,ylim=lim)
+  dev.off()
+  image_read(gsub("_map","_map1",path)) |> image_trim() |> image_border("x20",color="white") |> image_write(gsub("_map","_map1",path))
+  #file.show("C:/Users/God/Documents/floreqc/qcfloremap1.png")
+
+  png(gsub("_map","_map2",path),units="cm",width=3,height=1.5,res=500)
+  plotQC(region,ylim=NULL,cex=0.3)
+  dev.off()
+  image_read(gsub("_map","_map2",path)) |> image_trim() |> image_write(gsub("_map","_map2",path))
+
+  im1<-image_read(gsub("_map","_map1",path))
+  im2<-image_read(gsub("_map","_map2",path))
+  im<-image_composite(im1,image_scale(im2,"x140"),offset="+10+0",gravity="southeast")
+  im |> image_trim() |> image_write(path)
+
+  png(gsub("_map","_minimap",path),units="cm",width=3,height=1.5,res=500)
+  par(mar=c(0,0,0,0))
+  plot(st_geometry(region),lwd=0.1,col="grey80",border=NA)
+  plot(buff,col="red",border=NA,add=TRUE)
+  #plot(buffl,border="forestgreen",lwd=0.5,add=TRUE)
+  plot(st_geometry(region),lwd=0.1,col=NA,border="grey65",add=TRUE)
+  plot(st_geometry(lakes),col="grey99",border="grey75",lwd=0.05,add=TRUE)
+  plot(st_geometry(x),pch=21,bg=adjustcolor("forestgreen",0.75),col=adjustcolor("forestgreen",0.9),lwd=0.3,cex=0.35,add=TRUE)
+  dev.off()
+  image_read(gsub("_map","_minimap",path)) |> image_transparent(color="red",fuzz=5) |> image_trim() |> image_write(gsub("_map","_minimap",path))
+
+}
+stopCluster(cl)
+#plan(sequential)
+file.show(list.files(dirname(path),full=TRUE,pattern="_map.png")[1])
+
+
+
+herbier<-image_read("C:/Users/franc/Documents/floreqc/herbierqc.jpg")
+
+plot(herbier)
+
+png("C:/Users/franc/Documents/floreqc/overlayqc.png",res=300,units="in",width=3,height=3)
+par(mar=c(0,0,0,0),bg="transparent")
+plot(st_geometry(region),lwd=0.1,col="#ffffff",border=NA)
+plot(st_geometry(lakes),col="#ffffffff",border=NA,lwd=0.05,add=TRUE)
+dev.off()
+
+qc<-image_read("C:/Users/franc/Documents/floreqc/overlayqc.png")
+qc<-image_scale(qc,image_info(herbier)$height)
+
+im<-image_composite(herbier,qc,operator="blend",compose_args="90")
+image_write(im,"C:/Users/franc/Documents/floreqc/herbier.png")
