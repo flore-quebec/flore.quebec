988e5fe989010381889abb19a2dcd4f8dd3e6b85
frousseu <francois.rousseu@usherbrooke.ca>
2024-04-12 08:51:07 -0400

    minimal font size for popup

--- script.js
+++ script.js
@@ -777,7 +777,7 @@ et les supérieurs resserrés, ce qui lui donne une apparence unique.`;
                       .addTo(map2);
                       
                     let popupElem = popup.getElement();
-                    popupElem.style.fontSize = "1.1vh";  
+                    popupElem.style.fontSize = "clamp(12px, 1.3vh, 22px)";  
                     popupElem.style.fontFamily = "League Spartan";
                     popupElem.style.overflowWrap = "break-word";
                    
