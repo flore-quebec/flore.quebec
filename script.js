bb778c05c243b1496b24504564c5a37322f52995
frousseu <francois.rousseu@usherbrooke.ca>
2024-11-03 07:12:16 -0500

    change selected for taxon_name in remove div if species

--- script.js
+++ script.js
@@ -309,23 +309,8 @@ et les supérieurs resserrés, ce qui lui donne une apparence unique.`;
 
             if(group === "famille" || group === "genre" || group === "section") {
               document.getElementById("taxon_name").innerHTML = `${taxon} \u25BC`;
-              //<div class = "taxon_container" id = "taxon_container">
-                //<div class = "taxon_text">
-                //Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
-                //</div>
-                //<!-- <div class = "taxon_key">
-                //<h2>Clé d'identification</h2>
-//1 . Épillets pubescents et allongés ......................................... Espèce un<br>
-//1'. Épillets glabres et plus court ....................................................... 2<br>
-//2 . Épillets plus longs que larges ........................................ Espèce deux<br>
-//2'. Épillets plus larges que longs .........................................Espèce trois<br>
-//<br>
-//<br>
-                //</div> -->
-              //</div>
-              //`;
             } else {
-              document.getElementById("selected").innerHTML = "";
+              document.getElementById("taxon_name").innerHTML = "";
             }
 
 
